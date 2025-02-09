f4e8e581b3402135976f3277cd5713476b61816d
zoesolomon <130009623+zoesolomon@users.noreply.github.com>
2025-02-08 17:12:45 -0800

    us1-3 branch

---
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/MainViewModel.java
@@ -0,0 +1,15 @@
+package edu.ucsd.cse110.habitizer.app;
+
+import static androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY;
+
+import androidx.lifecycle.viewmodel.ViewModelInitializer;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MainViewModel extends ViewModel {
+
+
+}
--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/routine/RoutineView.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/routine/RoutineView.java
@@ -4,6 +4,21 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.ucsd.cse110.habitizer.app.MainViewModel;
 
 public class RoutineView extends Fragment {
 
