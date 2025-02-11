d59e2a82d739b6a7dff016806db4aad801d7c5c0
Pratyush-Bhattacharya <156235712+Pratyush-Bhattacharya@users.noreply.github.com>
2025-02-11 14:30:50 -0800

    Update Task.java

--- lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
+++ lib/src/main/java/edu/ucsd/cse110/habitizer/lib/domain/Task.java
@@ -11,32 +11,67 @@ package edu.ucsd.cse110.habitizer.lib.domain;
 public class Task {
     private String name;
     private boolean completed;
-
     private int timeSpent;
+
+    /**
+     * Task instantiates name as unique identifier for task.
+     *
+     * @param name Name for task.
+     */
     public Task(String name) {
         this.name = name;
         this.completed = false;
+        this.timeSpent = 0;
     }
+
+    /**
+     * Complete this task.
+     */
     public void completeTask() {
         completed = true;
     }
 
+    /**
+     * Returns if the task is true.
+     * 
+     * @return true if task is done, 
+     */
     public boolean isCompleted() {
         return completed;
     }
 
+    /**
+     * Returns if time spent on task.
+     * 
+     * @return timeSpent. 
+     */
     public int getTimeSpent() {
         return timeSpent;
     }
 
+    /**
+     * Sets timeSpent.
+     * 
+     * @param timeSpent to set timeSpent too. 
+     */
     public void setTime(int timeSpent) {
         this.timeSpent = timeSpent;
     }
 
+    /**
+     * Gets name.
+     * 
+     * @return name. 
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     * 
+     * @param name New name to set too. 
+     */
     public void setName(String name) {
         this.name = name;
     }
