92bbaa815fdbaa563478b8d884b59026ecb8a1f1
mmdavid1 <96997314+mmdavid1@users.noreply.github.com>
2025-02-12 15:32:55 -0800

    added homepage view

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_page);
+        setContentView(R.layout.homepage_view);
 
 
     }
