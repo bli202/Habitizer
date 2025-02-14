167d2efa7ec44d7b69522da88cba2e897b248291
mmdavid1 <96997314+mmdavid1@users.noreply.github.com>
2025-02-13 17:49:17 -0800

    more toggle completion tests

--- lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/TaskTest.java
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/domain/TaskTest.java
@@ -70,4 +70,28 @@ public class TaskTest {
         task.toggleCompletion();
         assertFalse("Task should not be completed after second toggle", task.isCompleted());
     }
+
+    @Test
+    public void testTaskToggleCompletionMultipleTasks() {
+        Task shower = new Task("Test");
+        Task gym = new Task("Test");
+        Task night = new Task("Test");
+
+        assertFalse("New task should not be completed", shower.isCompleted());
+        assertFalse("New task should not be completed", gym.isCompleted());
+        assertFalse("New task should not be completed", night.isCompleted());
+
+
+        shower.toggleCompletion();
+        // testing complete/non complete mix
+        assertTrue("Task should be completed after first toggle", shower.isCompleted());
+        assertFalse("Gym and night should still be false", gym.isCompleted());
+        assertFalse("Gym and night should still be false", night.isCompleted());
+
+        shower.toggleCompletion();
+        gym.toggleCompletion();
+        assertFalse("Task should not be completed after second toggle", shower.isCompleted());
+        assertTrue("Gym should be complete", gym.isCompleted());
+        assertFalse("Night should be complete", night.isCompleted());
+    }
 }
