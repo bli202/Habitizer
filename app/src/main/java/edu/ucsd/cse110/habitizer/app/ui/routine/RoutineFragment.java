6f4b65a72853a56665f86d75c3e0e30c99a319de
Aston Du <dankxiety@gmail.com>
2025-02-15 18:06:12 -0800

    added buttons

--- app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/routine/RoutineFragment.java
+++ app/src/main/java/edu/ucsd/cse110/habitizer/app/ui/routine/RoutineFragment.java
@@ -21,6 +21,8 @@ import java.util.ArrayList;
 import java.util.List;
 import android.graphics.Paint;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import edu.ucsd.cse110.habitizer.app.MainViewModel;
 import edu.ucsd.cse110.habitizer.app.R;
 import edu.ucsd.cse110.habitizer.app.ui.routine.dialog.AddTaskDialogFragment;
@@ -114,6 +116,8 @@ public class RoutineFragment extends Fragment {
 
         Button addTask = view.findViewById(R.id.add_task_button);
         Button startRoutine = view.findViewById(R.id.start_routine_button);
+        FloatingActionButton ffButton = view.findViewById(R.id.fast_forward_timer_button);
+        FloatingActionButton pauseTimerButton = view.findViewById(R.id.pause_timer_button);
 
         activityModel.getCompleted().observe(completed -> {
             if(completed == null) {
@@ -155,6 +159,16 @@ public class RoutineFragment extends Fragment {
 
                 }
             }.start();
+        });
+
+        ffButton.setOnClickListener(x -> {
+            if(!activityModel.getCurRoutine().getValue().getongoing()) return;
+            activityModel.getCurRoutine().getValue().manualAddTime(30);
+        });
+
+        pauseTimerButton.setOnClickListener(x -> {
+            if(!activityModel.getCurRoutine().getValue().getongoing()) return;
+            activityModel.getCurRoutine().getValue().pauseRoutineTimer();
         });
 
         if (getArguments() != null) {
