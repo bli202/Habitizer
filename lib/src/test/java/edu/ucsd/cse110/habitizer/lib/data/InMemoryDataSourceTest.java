9671f4dceff557c71160efacb7194297ebc522b2
Aston Du <dankxiety@gmail.com>
2025-02-16 18:48:45 -0800

    fixed inmemorydatasource tests

--- lib/src/test/java/edu/ucsd/cse110/habitizer/lib/data/InMemoryDataSourceTest.java
+++ lib/src/test/java/edu/ucsd/cse110/habitizer/lib/data/InMemoryDataSourceTest.java
@@ -11,15 +11,28 @@ import edu.ucsd.cse110.habitizer.lib.domain.Routine;
 import edu.ucsd.cse110.habitizer.lib.domain.Task;
 
 public class InMemoryDataSourceTest {
-    private static InMemoryDataSource data;
+//    private final InMemoryDataSource data = InMemoryDataSource.fromDefault();
 
-    @BeforeClass
-    public static void setup() {
-        data = InMemoryDataSource.fromDefault();
+    public InMemoryDataSource setup() {
+        InMemoryDataSource data = new InMemoryDataSource();
+        final Routine MORNING_ROUTINE = new Routine(30, "Morning Routine");
+        final Task BRUSH_TEETH = new Task("Brush teeth");
+        final Routine EVENING_ROUTINE = new Routine(45, "Evening Routine");
+        final Task WORK_OUT = new Task("Work out");
+        // Add default routine.
+        data.putRoutine(MORNING_ROUTINE);
+        data.putRoutine(EVENING_ROUTINE);
+        // Add default task to the default routine.
+
+        data.putTask(MORNING_ROUTINE.getTitle(), BRUSH_TEETH);
+        data.putTask(EVENING_ROUTINE.getTitle(), WORK_OUT);
+        return data;
     }
 
     @Test
     public void getRoutines() {
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
+        var data = setup();
         var routines = data.getRoutines();
         assertEquals(2, routines.size());
         assertEquals("Morning Routine", routines.get(1).getName());
@@ -28,6 +41,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void getRoutine() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         var routine = data.getRoutine("Morning Routine");
         assertEquals("Morning Routine", routine.getName());
         assertEquals(30, routine.getDuration());
@@ -36,6 +51,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void getRoutineSubject() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         var routineSubject = data.getRoutineSubject("Evening Routine");
         var value = routineSubject.getValue();
         assertEquals("Evening Routine", value.getName());
@@ -63,6 +80,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void removeRoutine() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         data.removeRoutine("Morning Routine");
         assertEquals(1, data.getRoutines().size());
         assertEquals("Evening Routine", data.getRoutines().get(0).getName());
@@ -74,6 +93,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void getTasksForRoutine() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         var tasks = data.getTasksForRoutine("Morning Routine");
         assertEquals(1, tasks.size());
         assertEquals("Brush teeth", tasks.get(0).getName());
@@ -81,6 +102,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void getRoutineTasksSubject() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         var tasksSubject = data.getRoutineTasksSubject("Evening Routine");
         var tasks = tasksSubject.getValue();
         assertEquals(1, tasks.size());
@@ -89,6 +112,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void putTask() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         Task newTask = new Task("new task");
         data.putTask("Morning Routine", newTask);
         var routine = data.getRoutine("Morning Routine");
@@ -98,6 +123,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void removeTask() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         data.removeTask("Evening Routine", "Work out");
         var routine = data.getRoutine("Evening Routine");
         assertEquals(0, routine.getTaskList().size());
@@ -105,6 +132,8 @@ public class InMemoryDataSourceTest {
 
     @Test
     public void editTask() {
+        var data = setup();
+//        InMemoryDataSource data = InMemoryDataSource.fromDefault();
         data.editTask("Morning Routine", "Brush teeth", "Don't brush teeth");
         var routine = data.getRoutine("Morning Routine");
         var task = data.getTasksForRoutine("Morning Routine").get(0);
